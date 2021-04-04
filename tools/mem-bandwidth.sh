43fdb6fae94ae20f0753501627ae465f39233675
Guodong Xu <guodong.xu@linaro.org>
2021-04-04 01:14:09 +0000

    tools: add base and mem-bandwidth testing scripts
    Signed-off-by: Guodong Xu <guodong.xu@linaro.org>

--- tools/mem-bandwidth.sh
+++ tools/mem-bandwidth.sh
@@ -4,16 +4,116 @@ set -x;
 source base.sh
 
 # stream
-CMD="/home/guodong/stream/Code/stream.100m.100.openmp"
-PARAM=""
+CMD="/usr/lib/lmbench/bin/stream"
+PARAM="-M 1024M -N 5"
 EXTRA_PARAM=""
 
 if [ 1 -eq 0 ]; then
-# Stream 4 threads， spread over 1 CCL/ 2CCL /3CCL /4CCL:
-OMP_NUM_THREADS=4 CPU_BINDINGS=("0-3" "0,1,4,5" "0,1,4,8" "0,4,8,12" "8,12,16,20") 
+# Stream 1 thread
+CPU_BINDINGS=(${CPU_BINDINGS_1[@]})
+EXTRA_PARAM="-P 1"
 # do it
-# for cpu_binding in "${CPU_BINDINGS[@]}"; do numactl --physcpubind=$cpu_binding $CMD $PARAM; done;
 doit "${CMD}" "${EXTRA_PARAM}" "${PARAM}" "${CPU_BINDINGS[@]}"
+
+# Stream 4 threads
+CPU_BINDINGS=(${CPU_BINDINGS_4[@]})
+EXTRA_PARAM="-P 4"
+# do it
+doit "${CMD}" "${EXTRA_PARAM}" "${PARAM}" "${CPU_BINDINGS[@]}"
+
+# Stream 6 threads
+CPU_BINDINGS=(${CPU_BINDINGS_6[@]})
+EXTRA_PARAM="-P 6"
+# do it
+doit "${CMD}" "${EXTRA_PARAM}" "${PARAM}" "${CPU_BINDINGS[@]}"
+
+# Stream 12 threads
+CPU_BINDINGS=(${CPU_BINDINGS_12[@]})
+EXTRA_PARAM="-P 12"
+# do it
+doit "${CMD}" "${EXTRA_PARAM}" "${PARAM}" "${CPU_BINDINGS[@]}"
+
+# Stream 24 threads
+CPU_BINDINGS=(${CPU_BINDINGS_24[@]})
+EXTRA_PARAM="-P 24"
+# do it
+doit "${CMD}" "${EXTRA_PARAM}" "${PARAM}" "${CPU_BINDINGS[@]}"
+
+# Stream 8 threads
+CPU_BINDINGS=(${CPU_BINDINGS_8[@]})
+EXTRA_PARAM="-P 8"
+# do it
+doit "${CMD}" "${EXTRA_PARAM}" "${PARAM}" "${CPU_BINDINGS[@]}"
+
+# Stream 16 threads
+CPU_BINDINGS=(${CPU_BINDINGS_16[@]})
+EXTRA_PARAM="-P 16"
+# do it
+doit "${CMD}" "${EXTRA_PARAM}" "${PARAM}" "${CPU_BINDINGS[@]}"
+
+# Stream 20 threads
+CPU_BINDINGS=(${CPU_BINDINGS_20[@]})
+EXTRA_PARAM="-P 20"
+# do it
+doit "${CMD}" "${EXTRA_PARAM}" "${PARAM}" "${CPU_BINDINGS[@]}"
+
+# Stream others, unbalanced configuration
+CPU_BINDINGS=("0-2,4")
+EXTRA_PARAM="-P 4"
+# do it
+doit "${CMD}" "${EXTRA_PARAM}" "${PARAM}" "${CPU_BINDINGS[@]}"
+CPU_BINDINGS=("0-3,4,8" "0-2,4-5,8")
+EXTRA_PARAM="-P 6"
+# do it
+doit "${CMD}" "${EXTRA_PARAM}" "${PARAM}" "${CPU_BINDINGS[@]}"
+
+# Cross NUMAs
+# 4 threads， spread over 1 NUMA / 2 NUMA / 3 NUMA / 4 NUMA nodes:
+CPU_BINDINGS=(${CPU_BINDINGS_4_NUMA[@]})
+EXTRA_PARAM="-P 4"
+# do it without membind
+doit_no_membind "${CMD}" "${EXTRA_PARAM}" "${PARAM}" "${CPU_BINDINGS[@]}"
+
+# 8 threads， spread over 1 NUMA / 2 NUMA / 3 NUMA / 4 NUMA nodes:
+CPU_BINDINGS=(${CPU_BINDINGS_8_NUMA[@]})
+EXTRA_PARAM="-P 8"
+# do it without membind
+doit_no_membind "${CMD}" "${EXTRA_PARAM}" "${PARAM}" "${CPU_BINDINGS[@]}"
+
+# 12 threads， spread over 1 NUMA / 2 NUMA / 3 NUMA / 4 NUMA nodes:
+CPU_BINDINGS=(${CPU_BINDINGS_12_NUMA[@]})
+EXTRA_PARAM="-P 12"
+# do it without membind
+doit_no_membind "${CMD}" "${EXTRA_PARAM}" "${PARAM}" "${CPU_BINDINGS[@]}"
+
+# 24 threads， spread over 1 NUMA / 2 NUMA / 3 NUMA / 4 NUMA nodes:
+CPU_BINDINGS=(${CPU_BINDINGS_24_NUMA[@]})
+EXTRA_PARAM="-P 24"
+# do it without membind
+doit_no_membind "${CMD}" "${EXTRA_PARAM}" "${PARAM}" "${CPU_BINDINGS[@]}"
+
+# 48 threads， spread over 2 NUMA / 3 NUMA / 4 NUMA nodes:
+CPU_BINDINGS=(${CPU_BINDINGS_48_NUMA[@]})
+EXTRA_PARAM="-P 48"
+# do it without membind
+doit_no_membind "${CMD}" "${EXTRA_PARAM}" "${PARAM}" "${CPU_BINDINGS[@]}"
+
+# 72 threads， spread over 3 NUMA / 4 NUMA nodes:
+CPU_BINDINGS=(${CPU_BINDINGS_72_NUMA[@]})
+EXTRA_PARAM="-P 72"
+# do it without membind
+doit_no_membind "${CMD}" "${EXTRA_PARAM}" "${PARAM}" "${CPU_BINDINGS[@]}"
+fi
+
+# CPU on NUMA0 ==> Memory on (NUMA0, NUMA1, NUMA2, or NUMA3)
+numactl -C 0,4,8,12 -m 0 /usr/lib/lmbench/bin/stream -P 4 -M 1024M -N 5
+numactl -C 0,1,4,5 -m 1 /usr/lib/lmbench/bin/stream -P 4 -M 1024M -N 5
+numactl -C 0,1,4,8 -m 1 /usr/lib/lmbench/bin/stream -P 4 -M 1024M -N 5
+numactl -C 0,4,8,12 -m 1 /usr/lib/lmbench/bin/stream -P 4 -M 1024M -N 5
+numactl -C 0,4,8,12 -m 2 /usr/lib/lmbench/bin/stream -P 4 -M 1024M -N 5
+numactl -C 0,4,8,12 -m 3 /usr/lib/lmbench/bin/stream -P 4 -M 1024M -N 5
+
+exit
 
 # Stream 8 threads， spread over 2CCL /3CCL /4CCL/ 5CCL /6CCLs:
 OMP_NUM_THREADS=8 CPU_BINDINGS=("0-7" "0-2,4-5,8-10" "0-1,4-5,8-9,12-13" "0-1,4,8-9,12,16-17" "0-1,4,8,12-13,16,20")
@@ -47,7 +147,6 @@ OMP_NUM_THREADS=72
 CPU_BINDINGS=("0-71" "0-17,24-41,48-65,72-89")
 # do it without membind
 doit_no_membind "${CMD}" "${EXTRA_PARAM}" "${PARAM}" "${CPU_BINDINGS[@]}"
-fi
 
 # lmbench/bw_mem
 for what in rd wr rdwr cp fwr frd fcp bzero bcopy
