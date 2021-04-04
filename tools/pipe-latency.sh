38907a35ecebdcb054b3fa6206b23ff2c32bf4e1
Guodong Xu <guodong.xu@linaro.org>
2021-04-04 07:47:52 +0000

    tools: pipe-latency.sh update
    Signed-off-by: Guodong Xu <guodong.xu@linaro.org>

--- tools/pipe-latency.sh
+++ tools/pipe-latency.sh
@@ -7,6 +7,7 @@ CMD="/home/guodong/lmbench-3.0-a9/bin/lat_pipe"
 PARAM="-W 1 -N 4"
 EXTRA_PARAM=""
 
+if [ 1 -eq 0 ]; then
 # Benchmark pipe latency between each and every pair of cores from CCL0 to CCL5
 #   , at stride of 4 (CCL size):
 doit "${CMD}" "${EXTRA_PARAM}" "${PARAM}" "${CPU_PAIR_0[@]}"
@@ -15,3 +16,10 @@ doit "${CMD}" "${EXTRA_PARAM}" "${PARAM}" "${CPU_PAIR_2[@]}"
 doit "${CMD}" "${EXTRA_PARAM}" "${PARAM}" "${CPU_PAIR_3[@]}"
 doit "${CMD}" "${EXTRA_PARAM}" "${PARAM}" "${CPU_PAIR_4[@]}"
 doit "${CMD}" "${EXTRA_PARAM}" "${PARAM}" "${CPU_PAIR_5[@]}"
+fi
+
+# Cross NUMAs
+PARAM="-W 1 -N 20"
+LMBENCH_SCHED="CUSTOM_SPREAD 0 24" ${CMD} ${PARAM}
+LMBENCH_SCHED="CUSTOM_SPREAD 0 48" ${CMD} ${PARAM}
+LMBENCH_SCHED="CUSTOM_SPREAD 0 72" ${CMD} ${PARAM}
